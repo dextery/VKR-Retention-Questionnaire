27d88e02db35b3e1c2b65dc00f98ede65e14283b
dextery <zupnikegor@gmail.com>
2026-06-07 22:58:53 +0300

    Added port message

--- backend/server.js
+++ backend/server.js
@@ -302,6 +302,6 @@ app.get('/dbtest', async (req, res) => {
 const PORT =
   process.env.PORT || 3001
 
-app.listen(PORT, () => {
- // console.log(`Server running on port ${PORT}`)
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server running on port ${PORT}`)
 })
